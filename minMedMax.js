5778989e491d04ac31940e476c1831d27d2a48ff
Kira <c3bosskung@gmail.com>
2022-09-01 02:45:48 +0700

    add checking for same value sorting

--- minMedMax.js
+++ minMedMax.js
@@ -1,23 +1,30 @@
-const { template } = require('@babel/core')
+//const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   let arr = [n1, n2, n3];
   let min, mid, max;
-  if (arr[0] < arr[1] && arr[0] < arr[2]) {
+  if (arr[0] <= arr[1] && arr[0] <= arr[2]) {
     min = arr[0];
-  }  if (arr[1] < arr[0] && arr[1] < arr[2] ) {
+  }  
+  if (arr[1] <= arr[0] && arr[1] <= arr[2] ) {
     min = arr[1];
-  }  if (arr[2] < arr[0] && arr[2] < arr[1]) {
+  }  
+  if (arr[2] <= arr[0] && arr[2] <= arr[1]) {
     min = arr[2];
-  }  if (arr[0] > arr[1] && arr[0] > arr[2]) {
+  }  
+  if (arr[0] >= arr[1] && arr[0] >= arr[2]) {
     max = arr[0];
-  }  if (arr[1] > arr[0] && arr[1] > arr[2] ) {
+  }  
+  if (arr[1] >= arr[0] && arr[1] >= arr[2] ) {
     max = arr[1];
-  }  if (arr[2] > arr[0] && arr[2] > arr[1]) {
+  }  
+  if (arr[2] >= arr[0] && arr[2] >= arr[1]) {
     max = arr[2];
   }
   for (let i = 0; i < arr.length; i++) {
     if(arr[i] != min && arr[i] != max) {
+      mid = arr[i]
+    } else if (arr[i] == arr[0] || arr[i] == arr[1]) {
       mid = arr[i]
     }
   }
@@ -28,6 +35,6 @@ function minMedMax(n1, n2, n3) {
   }
 }
 
-console.log(minMedMax(2, 5, 1));
+console.log(minMedMax(5, 1, 1));
 
-module.exports = minMedMax
+//module.exports = minMedMax
